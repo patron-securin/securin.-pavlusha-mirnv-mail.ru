1fbaa03139fb3e85c671070d54e3ff964d0f90f7
patron-securin <47637153+patron-securin@users.noreply.github.com>
2019-02-25 19:45:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ from io import BytesIO
 
 
 model = ClassPredictor()
+job_queue = Queue()
 
 
-def send_prediction_on_photo(bot, update):
-    chat_id = update.message.chat_id
-    print("Got image from {}".format(chat_id))
+def worker(bot, queue):
+    while True:
+        message = queue.get()
+        chat_id = message.chat_id
+        print("Got image from {}".format(chat_id))
 
-    # получаем информацию о картинке
-    image_info = update.message.photo[-1]
-    image_file = bot.get_file(image_info)
-    image_stream = BytesIO()
-    image_file.download(out=image_stream)
+        # получаем информацию о картинке
+        image_info = message.photo[-1]
+        image_file = bot.get_file(image_info)
+        image_stream = BytesIO()
+        image_file.download(out=image_stream)
 
-    class_ = model.predict(image_stream)
+        # симулируем долгую работу сети, чтобы показать, почему нужен отдельный процесс и как это работает
+        class_ = 1#model.predict(image_stream)
+        sleep(10)
 
-    # теперь отправим результат
-    update.message.reply_text(str(class_))
-    print("Sent Answer to user, predicted: {}".format(class_))
+        # теперь отправим результат
+        message.reply_text(str(class_))
+        print("Sent Answer to user, predicted: {}".format(class_))
+
+
+def photo(bot, update):
+    update.message.reply_text("Ваше фото помещено в очередь")
+    job_queue.put(update.message)
 
 
 if __name__ == '__main__':
@@ -37,5 +47,11 @@ if __name__ == '__main__':
         level=logging.INFO)
     # используем прокси, так как без него у меня ничего не работало(
     updater = Updater(token=token)
-    updater.dispatcher.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
+    updater.dispatcher.add_handler(MessageHandler(Filters.photo, photo))
+
+    # Сделаем отдельный процесс для того, чтобы обрабатывать картинки
+    worker_args = (updater.bot, job_queue)
+    worker_process = Process(target=worker, args=worker_args)
+    worker_process.start()
+
     updater.start_polling()
