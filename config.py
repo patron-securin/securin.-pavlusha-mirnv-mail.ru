b572937dbb65a29a44e8a9942d9fc208b98135b5
patron-securin <47637153+patron-securin@users.noreply.github.com>
2019-02-26 12:34:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-reply_texts = {
-    'pred_answer': 'I predicted class with index {}. To show class name you will need to extract it from' +
-                   'dataloader you used while training'
-}
